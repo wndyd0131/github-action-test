8d81d42665a7cc7c204cce5e105d8a46ede93a3d
wndyd <wndyd0131@naver.com>
2023-04-29 15:17:53 +0900

    action docker test1

--- src/main/java/hello/core/annotation/MainDiscountPolicy.java
+++ src/main/java/hello/core/annotation/MainDiscountPolicy.java
@@ -11,3 +11,5 @@ import java.lang.annotation.*;
 @Qualifier("mainDiscountPolicy") // Qualifier 쓸 때 문자열이 아닌 어노테이션으로 사용하도록 해줌 (컴파일 에러 유도를 위함)
 public @interface MainDiscountPolicy {
 }
+
+//c
---
+++ src/main/java/hello/core/controller/SampleController.java
@@ -0,0 +1,12 @@
+package hello.core.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+    @RequestMapping("/sample")
+    public String sample() {
+        return "Sample!!";
+    }
+}
